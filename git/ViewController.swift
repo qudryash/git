dff6b000fc28121349aeb8cd1f0476d03fc86374
qudryash <qudryashovav@yandex.ru>
2023-11-06 16:05:26 +0700

    Главная ветка G

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloLabel.text = "G"
     }
 
 
