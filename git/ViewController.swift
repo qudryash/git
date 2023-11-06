17e0769ab650a72443d46667bca3ffd8b5523e4e
qudryash <qudryashovav@yandex.ru>
2023-11-06 16:30:12 +0700

    Добавлено G1.3

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        helloLabel.text = "G1.2"
+        helloLabel.text = "G1.3"
     }
 
 
